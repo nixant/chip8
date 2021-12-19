7aaf12b83ddd68a74f8457374946b3d289f7b757
Nishant Kaushik <nixantkaushik@gmail.com>
2021-12-19 17:09:21 +0530

    Added accessors for timers

--- src/Emulator/Chip8/Timers.hs
+++ src/Emulator/Chip8/Timers.hs
@@ -2,7 +2,7 @@
 
 module Emulator.Chip8.Timers where
 
-import Control.Concurrent.STM (TVar, atomically, modifyTVar', readTVarIO)
+import Control.Concurrent.STM (TVar, atomically, modifyTVar', readTVarIO, writeTVar)
 import Data.Default
 import Data.Word (Word8)
 
@@ -40,8 +40,17 @@ tickTimers (Timers d s) = Timers (tickTimer d) (tickTimer s)
 
 class HasTimers a where
   tick :: a -> IO ()
+  getDT :: a -> IO Word8
+  setDT :: a -> Word8 -> IO ()
+  getST :: a -> IO Word8
+  setST :: a -> Word8 -> IO ()
 
 instance HasTimers (TVar Timers) where
   tick t =
     atomically (modifyTVar' t tickTimers) >> readTVarIO t >>= \(Timers d s) ->
       timerAction d (timer d) >> timerAction s (timer s)
+  getDT = fmap (timer . dt) . readTVarIO
+  setDT t d = atomically $ modifyTVar' t (\(Timers dt st) -> Timers dt {timer = d} st)
+  getST = fmap (timer . st) . readTVarIO
+  setST t d = atomically $ modifyTVar' t (\(Timers dt st) -> Timers dt st {timer = d})
+
